d2b5f8f03c8adab3f50b25e439d4eb4a1afa01db
Iury Vasconcelos <iury0393@gmail.com>
2020-06-29 15:07:43 -0300

    Testando

--- src/components/Menu-item/menu-item.component.jsx
+++ src/components/Menu-item/menu-item.component.jsx
@@ -10,7 +10,6 @@ const MenuItem = ({ title, subtitle, imageUrl }) => (
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">{subtitle}</span>
-      
     </div>
   </div>
 );
