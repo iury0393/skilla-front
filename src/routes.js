9981705d4e6a36539131c15ab76b06c6b63d645c
Iury Vasconcelos <iury0393@gmail.com>
2020-06-29 10:08:21 -0300

    Subindo os estilos

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// pages, components, styles
 import Nav from "./components/Nav";
 import Container from "./styles/Container";
 import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Explore from "./pages/Explore";
+import DetailedPost from "./pages/DetailedPost";
+import EditProfile from "./pages/EditProfile";
 
 const Routing = () => {
   return (
@@ -11,6 +16,10 @@ const Routing = () => {
       <Nav />
       <Container>
         <Switch>
+          <Route path="/explore" component={Explore} />
+          <Route path="/p/:postId" component={DetailedPost} />
+          <Route path="/accounts/edit" component={EditProfile} />
+          <Route path="/:username" component={Profile} />
           <Route path="/" component={Home} />
         </Switch>
       </Container>
