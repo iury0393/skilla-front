e0f8f8f38fa5caf475c7d98edc54ba9d8a7daafb
Iury Vasconcelos <iury0393@gmail.com>
2020-06-29 19:25:03 -0300

    Mudando o estilo do texto

--- src/pages/Homepage/homepage.component.jsx
+++ src/pages/Homepage/homepage.component.jsx
@@ -6,8 +6,8 @@ import Trophy from '../../assets/trophy.svg';
 const HomePage = () => (
   <div className="homepage">
     <img className="icone-principal" src={Trophy} alt=""/>
-    <p>Skilla é um projeto feito por alunos da unifor para estagiários<br />Terem chance no mercado de TI.</p>
-    <p>O que oferecemos na aplicação:</p>
+    <p className="text-principal">Skilla é um projeto feito por alunos da unifor para estagiários<br />Terem chance no mercado de TI.</p>
+    <p className="text-principal">O que oferecemos na aplicação:</p>
     <Directory/>
   </div>
 );
--- src/utils/index.js
+++ src/utils/index.js
@@ -67,7 +67,7 @@ export const client = (endpoint, { body, ...customConfig } = {}) => {
 export const uploadImage = file => {
 	const data = new FormData();
 	data.append("file", file);
-	data.append("upload_preset", "instaclone");
+	data.append("upload_preset", "otwdwv0m");
 
 	return fetch(process.env.REACT_APP_CLOUDINARY_URL, {
 		method: "POST",
